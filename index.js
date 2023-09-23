fa225dcb31b37d9a8b3a44dfaa86091ce8c3ce33
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-09-23 15:31:27 +0300

    modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let currentUser ='user1';
-let welcomeMessage =`Welcome to Flatbook, ${currentUser} !`;
-let excitedWelcomeMessage =`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`;
-let shortGreeting=`Welcome, ${currentUser[0]} !`;
+let welcomeMessage ='Welcome to Flatbook, '+ currentUser +' !';
+let excitedWelcomeMessage ='WELCOME TO FLATBOOK, ' +currentUser.toUpperCase()+ '!';
+let shortGreeting='Welcome, '+currentUser[0] +' !';
